5b4b3c0af1949dd1165d37106f62fd4dc607b622
Phoenix-star-tech <phoenixsoftwaresolutions172@gmail.com>
2025-09-20 16:14:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,20 @@ app.secret_key = "secret123"
 scope = ["https://spreadsheets.google.com/feeds",
          "https://www.googleapis.com/auth/drive"]
 
+creds_dict = {
+    "type": "service_account",
+    "project_id": "ultra-ridge-472307-j6",
+    "private_key_id": "d309bc3a5aa75e467bcc6f303159578ca31cf72d",
+    "private_key": "-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC3FBX9ZYb5sMfz\\nEJznKlGAluC69IxskPh7lzwb+OQ1sGNkyqiEuV5pI0JPf8x3O3GClQ/93FPr4uMB\\nm3PpopepQ7ydj/uBWDE1rk4i1txcWqX0NEgzKb77r5T9S3nDgas7I/nP7aB3hWZ3\\nrCHSOIEK1AEOx/rdYA8nvGL5bXyNJNgVdonKInwS85hJvE36rkBfJ/4RVAEVZz48\\niDDMjvSIX4T6IhXKlK0bW2gdUJUagtrsGVJBdYTq6vxAkxxNka7tl/4oJY4OsY61\\nmLLNFdneJa/UogHo1kP2i6RbHNoy7MM8TfFuY+8iIUhuHGAcYeaONNLPBf495BAL\\nHw4PEonZAgMBAAECggEATEGEBUdgKKmPJzmQuSKhzwdJNOX+hYv+y+/K3DXXzFsb\\nk1RsS9gwMGe8y4NGRZWPap4vCoTWucaH2lgHOFX+iMTLj+90iUQUPzFKn21SXO9u\\n92A9gH4PbCMO0lA5OAtPKLOL7qUiNRZt/0PF1JuveDSk+iSm48WKmnj32r/VZK4x\\nHzvR4m5yIDqOimjHce/2GEanMrxd1DM6r9ZWWkr/2LoiEW7PTlGK4hvDeVcaoJ+l\\n1Xg5roGkO4N5tjOGeB7OoaDibD0WlLXtXriIUmIj0hiK1tWfqIMjdxZVeY28WXxt\\n2LP443k/z/1LOSR2DOfTd5REaEImf7VdBG7oeUJdVwKBgQD70y/5CKi/v80MFg7p\\nqiNZLTbPZQiTS8ppSFFwgZ50JYqoMMgzro81KDjrKlyt02/ysC2ke7+XdvAwPqz+\\nBBCQJZUb/pP36semuyJD6pLBzcaDytZLkRiQIgA94bnMB35ipWfVJRXkJ3uygMMe\\nL3x2awwbTeyJMWQFG9k5Uur30wKBgQC6HR62P0tbAF4x8ABomTD4lt4PfeGvWEla\\nc4bVhNnVTZXiqn+GXweXP3ZLjaicfBmagvWkQ+//3spG5aK4fGgIBWjsAh6R46GP\\n975u+FJCk9wAHx4IonydFz+daZP9zrFWvu+WFLOXa1fQu8FaaIUokaLZO4lXPxPr\\ngEJ4RQG4IwKBgE7e8BGBYsjbm68DYZxRi0ys2pPOQwdPH5Al9EvWbPhXobvXu3xk\\nWbn+ZQVSeUCADnnmMAVqNLGNsOCLYMcWItHi03a3H0TwpaNUzQCUgW5tspUofEGi\\nqhzKaWT4Q6bhWfYvc/vP57FTpGxd476ahNLZ6CrNFx5I99iZxkkr7Se9AoGAZavm\\nzTTCm9IVoz4zWvDlGciBQwFHuxyF5g3aiOZsgeRCj3MI+4UKhou6ugeHJwV8jzYf\\nrz8V6zfwVM66GAKOamNaMCPwQ06RQi7bL5AkkA5qTv6wz5LEdKTwsbOtjyfNJVBl\\nXh2tBnkzneoT9KnIpKL6zaWCn9Drtul4Zm98QvUCgYEAxG0LiRyJY179dhxGj4CW\\noXRmUqaNViUGA93D+bgkefCF+zrW54qNhJDMj+o3+szLFz2oYkDbjPsQYH/18M3G\\ncozYCuuVOeX0iyZ6d+ILJWCE+LmINMmAngBXDT7QtOmATm24JVzcJhub9h2U2gMz\\nb0OYlvApi5xZKPbhOWAsE5I=\\n-----END PRIVATE KEY-----\\n",
+    "client_email": "attendance-service@ultra-ridge-472307-j6.iam.gserviceaccount.com",
+    "client_id": "117548735903945328880",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/attendance-service%40ultra-ridge-472307-j6.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
+
 # Load credentials from environment variable
 creds_json = os.environ.get("GOOGLE_CREDS")  # Set this in Render or .env locally
 if not creds_json:
@@ -20,7 +34,6 @@ if not creds_json:
 creds_dict = json.loads(creds_json)
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
-# -----------------------------------------------------
 
 # Map department → branch → year → sheet_id
 sheet_map = {
@@ -131,3 +144,4 @@ def submit_absent():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
